1c50f63c57c7d3291fd2045546d41981e9484466
haruki-3281 <haruharuharu0328@yahoo.co.jp>
2022-04-29 14:08:55 +0900

    eslint,prettierセットアップ

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,20 @@ module.exports = {
         "es2021": true
     },
     "extends": [
+        "react-app",
+        "react-app/jest",
         "plugin:react/recommended",
-        "airbnb"
+        "airbnb",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:import/typescript",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier",
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
+        "project":"./tsconfig.json",
         "ecmaFeatures": {
             "jsx": true
         },
@@ -17,8 +26,48 @@ module.exports = {
     },
     "plugins": [
         "react",
-        "@typescript-eslint"
+        "@typescript-eslint",
+        "import"
     ],
     "rules": {
+        "no-use-before-define":"off",
+        "@typescript-eslint/no-use-before-define":[
+            "error"
+        ],
+        "react/jsx-filename-extension":[
+            "error", 
+            { "extensions" :["js","jsx","ts","tsx"] }
+        ],
+        "react/jsx-pascal-case": [
+            2, 
+            { 
+                "allowAllCaps": false, 
+                "allowNamespace": false, 
+                "allowLeadingUnderscore": false, 
+                "ignore": ["$*"]
+            }
+        ],
+        "import/extensions":[
+            "error",
+            "ignorePackages",
+            {
+                "js": "never",
+                "jsx": "never",
+                "ts": "never",
+                "tsx": "never"
+            }
+        ]
+    },
+    "settings": {
+        "import/resolver": {
+            "node": {
+                "extensions" :[
+                    ".js",
+                    ".jsx",
+                    ".ts",
+                    ".tsx"
+                ]
+            }
+        }
     }
 }
